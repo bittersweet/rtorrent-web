a239691de5a8ce94536647f3ff63057e654d7b53
Mark Mulder <markmulder@gmail.com>
2015-03-26 11:46:41 +0100

    Update on every request

--- rtorrent.go
+++ rtorrent.go
@@ -32,28 +32,31 @@ type Torrent struct {
 	Hash              string
 }
 
-func handleIndex(torrents []Torrent) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		defer trackTime(time.Now(), "handleIndex")
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	defer trackTime(time.Now(), "handleIndex")
 
-		output, err := json.MarshalIndent(&torrents, "", "  ")
-		if err != nil {
-			log.Fatal("MarshalIndent", err)
-		}
+	torrents := getTorrents()
 
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		w.Write(output)
+	output, err := json.MarshalIndent(&torrents, "", "  ")
+	if err != nil {
+		log.Fatal("MarshalIndent", err)
 	}
-	return http.HandlerFunc(fn)
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Write(output)
 }
 
-func main() {
-	defer trackTime(time.Now(), "xmlRpc")
-	client, err := xmlrpc.NewClient("http://192.168.2.7:5001/RPC2", nil)
+var client *xmlrpc.Client
+
+func init() {
+	var err error
+	client, err = xmlrpc.NewClient("http://192.168.2.7:5001/RPC2", nil)
 	if err != nil {
 		log.Fatal("error: ", err)
 	}
+}
 
+func getTorrents() []Torrent {
 	var output [][]interface{}
 	if err := client.Call("d.multicall", []interface{}{"main", "d.name=", "d.bytes_done=", "d.connection_current=", "d.creation_date=", "d.get_down_rate=", "d.get_down_total=", "d.size_bytes=", "d.size_files=", "d.state=", "d.load_date=", "d.ratio=", "d.get_up_rate=", "d.get_up_total=", "d.hash="}, &output); err != nil {
 		fmt.Println("d.multicall call error: ", err)
@@ -84,9 +87,13 @@ func main() {
 	}
 
 	fmt.Printf("%#v\n", torrents[0])
+	return torrents
+}
 
-	mux := http.NewServeMux()
-	mux.Handle("/", handleIndex(torrents))
+func main() {
+	defer trackTime(time.Now(), "xmlRpc")
+
+	http.HandleFunc("/", handleIndex)
 	fmt.Println("Will start listening on port 8000")
-	http.ListenAndServe(":8000", mux)
+	http.ListenAndServe(":8000", nil)
 }
