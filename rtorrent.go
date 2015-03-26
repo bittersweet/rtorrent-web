886887ecdaad5b631aa9ea0997120a16c5cee47e
Mark Mulder <markmulder@gmail.com>
2015-03-26 11:18:12 +0100

    map torrents to struct

--- rtorrent.go
+++ rtorrent.go
@@ -13,23 +13,56 @@ func trackTime(start time.Time, name string) {
 	log.Printf("%s took %s\n", name, elapsed)
 }
 
+type Torrent struct {
+	Name              string
+	BytesDone         int64
+	ConnectionCurrent string
+	CreationDate      int64
+	GetDownRate       int64
+	GetDownTotal      int64
+	SizeBytes         int64
+	SizeFiles         int64
+	State             string
+	LoadDate          int64
+	Ratio             int64
+	GetUpRate         int64
+	GetUpTotal        int64
+	Hash              string
+}
+
 func main() {
 	defer trackTime(time.Now(), "xmlRpc")
 	client, err := xmlrpc.NewClient("http://192.168.2.7:5001/RPC2", nil)
 	if err != nil {
 		log.Fatal("error: ", err)
 	}
-	var version string
-	err = client.Call("system.api_version", nil, &version)
-	if err != nil {
-		log.Fatal("error: ", err)
-	}
-	fmt.Printf("Version: %s\n", version)
 
 	var output [][]interface{}
-	if err := client.Call("d.multicall", []interface{}{"main", "d.bytes_done=", "d.connection_current=", "d.creation_date=", "d.get_down_rate=", "d.get_down_total=", "d.size_bytes=", "d.size_files=", "d.state=", "d.load_date=", "d.name=", "d.ratio=", "d.get_up_rate=", "d.get_up_total=", "d.hash="}, &output); err != nil {
-
-		fmt.Println("service.sum call error: ", err)
+	if err := client.Call("d.multicall", []interface{}{"main", "d.name=", "d.bytes_done=", "d.connection_current=", "d.creation_date=", "d.get_down_rate=", "d.get_down_total=", "d.size_bytes=", "d.size_files=", "d.state=", "d.load_date=", "d.ratio=", "d.get_up_rate=", "d.get_up_total=", "d.hash="}, &output); err != nil {
+		fmt.Println("d.multicall call error: ", err)
 	}
-	fmt.Println(output)
+	// fmt.Println(output)
+	fmt.Println(len(output))
+	// fmt.Println(output[1])
+
+	torrents := make([]Torrent, len(output))
+	for i := 0; i < len(output); i++ {
+		data := output[i]
+
+		torrent := Torrent{
+			Name:              data[0].(string),
+			BytesDone:         data[1].(int64),
+			ConnectionCurrent: data[2].(string),
+			CreationDate:      data[3].(int64),
+			GetDownRate:       data[3].(int64),
+		}
+		// fmt.Println(torrent)
+		// fmt.Println(i)
+		// torrents = append(torrents, torrent)
+		torrents[i] = torrent
+	}
+
+	fmt.Printf("length: %#v\n", len(torrents))
+	fmt.Printf("%#v\n", torrents[0])
+	fmt.Printf("%#v\n", torrents[166])
 }
